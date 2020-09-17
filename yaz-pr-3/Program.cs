1719c3e1ebc43688717471ca7fe651d65a487e82
gorka1369 <Tancor@icloud.com>
2020-09-17 16:52:27 +0300

    Добавьте файлы проекта.

---
+++ yaz-pr-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace yaz_pr_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"boot.int");
+            StreamWriter sw = new StreamWriter("boot-utf7.txt", false, Encoding.UTF7);
+            sw.WriteLine(sr.ReadToEnd ());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
